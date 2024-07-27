a0af0ab1668cc5fca271b7790321ddee959d7171
Banderan <bartoszwojcik2004@gmail.com>
2024-07-27 11:37:05 +0200

    adding swagger

--- src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
+++ src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
@@ -1,6 +1,9 @@
 package mate.academy.rickandmorty.controller;
 
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.dto.internal.OutputCharacterResponseDto;
 import mate.academy.rickandmorty.service.CharacterService;
@@ -9,17 +12,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Character management",
+        description = "Endpoints for managing characters")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/animation")
+@RequestMapping("/character")
 public class CharacterController {
     private final CharacterService animationService;
 
+    @Operation(
+            summary = "Retrieve character",
+            description = "Get a character with random id from "
+                    + "rick & morty"
+    )
     @GetMapping
     public OutputCharacterResponseDto getCharacter() {
         return animationService.findCharacterByRandomId();
     }
 
+    @Operation(
+            summary = "searching characters",
+            description = "get a character with your parameters"
+                    + "u can search by letter or full name"
+    )
     @GetMapping("/search")
     public List<OutputCharacterResponseDto> getWithLettersInName(
             @RequestParam String name) {
